e3cef445febdd7f3c5ab69ac7f207ced855a10e9
840 <23346008+840@users.noreply.github.com>
2024-05-03 23:01:53 +0200

    .

---
+++ src/components/board.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+const board: string[][] = []
+const width = 10
+const height = 10
+
+for (let w = 0; w < width; w++) {
+    board[w] = []
+    for (let h = 0; h < height; h++) {
+        board[w][h] = "*"
+    }
+}
+</script>
+
+<div class="flex col bg-black" style="width: 100px; height: 100px;">
+    {#each board as row}
+        <div class="flex bg-black">
+            {#each row as tile}
+                <div class="cell bg-white w-[10px] h-[10px]"></div>
+            {/each}
+        </div>
+    {/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import Board from "../components/board.svelte";
+</script>
+
+<h1>Snek Game</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h2> 
+</h2>
+
+<Board/>
+
